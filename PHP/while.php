5f27857605764eacde940c6bdd7153048ab95dc3
Zeshan Segal <ZNS@Zeshans-MacBook-Air.local>
2015-11-04 09:56:24 -0600

    While loop and do _while loops

--- PHP/while.php
+++ PHP/while.php
@@ -2,8 +2,28 @@
 
 $test = 5;
 while ($test <= 15) {
-	echo "\$test is equal to {$test}\n";
+	echo "\$test is equal to {$test}" . PHP_EOL;
 	++$test;
 }
+
+
+
+$evenNum = 1;
+while ($evenNum <= 100) {
+	if($test % 2 == 0){
+	echo "\$evenNum is equal to {$evenNum}" . PHP_EOL;	
+	}
+	$evenNum += 1;
+}
+
+$oddNum = 1;
+while ($oddNum <= 100) {
+	echo "\$oddNum is equal to {$oddNum}" . PHP_EOL;
+	$oddNum += 2;
+}
+
+
+
+
 
 ?>
