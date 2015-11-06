cd22ea30e886717ebe9890c6ff9b698d47fc88f1
Zeshan Segal <zeshan.segal@gmail.com>
2015-11-06 15:00:20 -0600

    added in completed internal functions and tech company excercises

--- PHP/internal_functions.php
+++ PHP/internal_functions.php
@@ -1,35 +1,32 @@
 <?php
-// Your inspect() function should look at the passed argument and return a string with the variable's type and its value, similar to "The integer is 12.".
-// Use your new function to inspect the given variables one at a time. Create these calls in an iterative manner: add a call to inspect() for $num1 and then test your code, add a call to inspect() for $num2 and then test your code, etc.
-
-// There are some special cases your inpsect() function needs to handle. In particular:
-
-####// If the variable is NULL, return "The value is NULL."
-####// If the variable is an array, return "The value is an array"
-####// If the variable is an empty array, return "The value is an empty array."
-####// If the variable is a boolean, make sure your return value says whether it is TRUE or FALSE.
-// If the variable is an empty string, return "The string is empty."
 
 function inspect($value)
 {
-	switch (gettype($value)) {
-		case 'null':
-			return "The value is NULL.";
+	switch (gettype($value)) 
+	{
+		case 'NULL': 
+			return "The value is NULL" . PHP_EOL;
 			break;
 		case 'array':
-			if ($value == []){
-				return "The value is an empty array.";
-			} else {
-				return "The value is an array.";			
+			if ($value == [])
+			{
+				return "The value is an empty array." . PHP_EOL;
+			} 
+			else 
+			{
+				return "The value is an array with $value[0], $value[1], and $value[2]." . PHP_EOL;			
 			}
 		case 'boolean':
-			if ($value == 1){
-				return "Value is a Boolean that is TRUE";
-			} else {
-				return "Value is a Boolean that is FALSE";
+			if ($value == 1)
+			{
+				return "This is a Boolean that is TRUE" . PHP_EOL;
+			} 
+			else 
+			{
+				return "This is a Boolean that is FALSE" . PHP_EOL;
 			}
 		case 'string':
-			return "The value of the string is {$value)"; 	
+			return "The value of the string is " . "'$value'" . PHP_EOL; 	
 		default:
 			return "The " . gettype($value) . " is {$value}" . PHP_EOL;
 			break;
@@ -53,23 +50,34 @@ $null = NULL;
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
 
 echo 'Inspecting $num1:' . PHP_EOL;
+echo inspect($num1) . PHP_EOL; 
 
 echo 'Inspecting $num2:' . PHP_EOL;
+echo inspect($num2) . PHP_EOL;
 
 echo 'Inspecting $num3:' . PHP_EOL;
+echo inspect($num3) . PHP_EOL;
 
 echo 'Inspecting $num4:' . PHP_EOL;
+echo inspect($num4) . PHP_EOL;
 
 echo 'Inspecting $null:' . PHP_EOL;
+echo inspect($null) . PHP_EOL;
 
 echo 'Inspecting $bool1' . PHP_EOL;
+echo inspect($bool1) . PHP_EOL;
 
 echo 'Inspecting $bool2' . PHP_EOL;
+echo inspect($bool2) . PHP_EOL;
 
 echo 'Inspecting $string1' . PHP_EOL;
+echo inspect($string1) . PHP_EOL;
 
 echo 'Inspecting $string2' . PHP_EOL;
+echo inspect($string2) . PHP_EOL;
 
 echo 'Inspecting $array1' . PHP_EOL;
+echo inspect($array1) . PHP_EOL;
 
 echo 'Inspecting $array2' . PHP_EOL;
+echo inspect($array2) . PHP_EOL;
